e4a4b5c1e973b41e6bafaf9eab2e5bbe87daf80c
Emanuelle Ferraz <111248050+emanuelleferraz@users.noreply.github.com>
2023-09-08 19:37:50 -0300

    .

--- Magazine Hashtag/main.js
+++
@@ -1,9 +0,0 @@
-import { renderizarCatalogo } from "./src/cartaoProduto";
-import { inicializarCarrinho, atualizarPrecoCarrinho,  renderizarProdutosCarrinho} from "./src/menuCarrinho";
-import { inicializarFiltros } from "./src/filtrosCatalogo";
-
-renderizarCatalogo();
-inicializarCarrinho();
-renderizarProdutosCarrinho();
-atualizarPrecoCarrinho();
-inicializarFiltros();
