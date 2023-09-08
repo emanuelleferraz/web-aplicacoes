27d1f0430ddb2ded5d98fb92d9403497a26e167a
Emanuelle Ferraz <111248050+emanuelleferraz@users.noreply.github.com>
2023-09-08 20:00:59 -0300

    e-commerce magazine

---
+++ Magazine Hashtag/main.js
@@ -0,0 +1,10 @@
+import { renderizarCatalogo } from "./src/cartaoProduto";
+import { inicializarCarrinho, atualizarPrecoCarrinho, renderizarProdutosCarrinho } from "./src/menuCarrinho";
+import { inicializarFiltros } from "./src/filtrosCatalogo";
+
+renderizarCatalogo();
+inicializarCarrinho();
+renderizarProdutosCarrinho();
+atualizarPrecoCarrinho();
+inicializarFiltros();
+inicializarFiltros;
